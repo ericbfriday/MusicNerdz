02e5cb1eba79f5d842509b7453118a7b50b044b5
Eric Friday <ericfriday@gmail.com>
2017-11-04 13:13:24 -0500

    Refactored variables, classes, and functions for quiz creation to go into service. Route not yet established.

--- server/public/scripts/controllers/quizCreation.controller.js
+++ server/public/scripts/controllers/quizCreation.controller.js
@@ -1,4 +1,4 @@
-myApp.controller('QuizCreation', function ($http, $scope, $mdDialog) {
+myApp.controller('QuizCreation', function (ModuleCreation) {
     // console.log('in home controller');
 
     // Code Readability notes:
@@ -11,133 +11,36 @@ myApp.controller('QuizCreation', function ($http, $scope, $mdDialog) {
 
     const vm = this;
 
-    vm.quiz = {data: []};
-    vm.name = '';
-    vm.currentQType = '';
-    vm.questions = []; // holds questions to push into quiz
-    vm.currentMCQ = ''; // MC Question
-    vm.currentMCA1 = ''; // MC Answers 1-4 below
-    vm.currentMCA2 = '';
-    vm.currentMCA3 = '';
-    vm.currentMCA4 = '';
-    vm.currentMCCA = ''; // MCCA = Multiple Choice Correct Answer
-    vm.currentSAQ = ''; // SA Question
-    vm.currentSAA = ''; // SA Answer/hints to grade on
-    vm.currentEQ = ''; // Essay Question/Topic
-    vm.currentEA = ''; // Essay Answer/hints to grade on
-
-    // establishes structure for multiple choice question to be inserted into questions array
-    class MCQuestion {
-        constructor (q, a1, a2, a3, a4, ca){
-            this.q = q;
-            this.a1 = a1;
-            this.a2 = a2;
-            this.a3 = a3;
-            this.a4 = a4;
-            this.ca = ca; // ca = correct answer
-            this.type = 'mc';
-        }
-    } // end MCQuestion class
+    vm.quiz = ModuleCreation.quiz;
+    vm.name = ModuleCreation.name;
+    vm.currentQType = ModuleCreation.currentQType;
+    vm.questions = ModuleCreation.questions; // holds questions to push into quiz
+    vm.currentMCQ = ModuleCreation.currentMCQ; // MC Question
+    vm.currentMCA1 = ModuleCreation.currentMCA1; // MC Answers 1-4 below
+    vm.currentMCA2 = ModuleCreation.currentMCA2;
+    vm.currentMCA3 = ModuleCreation.currentMCA3;
+    vm.currentMCA4 = ModuleCreation.currentMCA4;
+    vm.currentMCCA = ModuleCreation.currentMCAA; // MCCA = Multiple Choice Correct Answer
+    vm.currentSAQ = ModuleCreation.currentSAQ; // SA Question
+    vm.currentSAA = ModuleCreation.currentSAA; // SA Answer/hints to grade on
+    vm.currentEQ = ModuleCreation.currentEQ; // Essay Question/Topic
+    vm.currentEA = ModuleCreation.currentEA; // Essay Answer/hints to grade on
 
     // pushes MC Q into Q array
-    vm.pushMCQ = (q, a1, a2, a3, a4, ca) => {
-        vm.newMCQ = new MCQuestion(q, a1, a2, a3, a4, ca);
-        vm.questions.push(vm.newMCQ);
-        console.log('loggin vm.newMCQ in pushMCQuestion -> ', vm.newMCQ);
-
-        // clear the form for the next questions submission
-        vm.currentMCQ = '';
-        vm.currentMCA1 = '';
-        vm.currentMCA2 = '';
-        vm.currentMCA3 = '';
-        vm.currentMCA4 = '';
-        vm.currentMCCA = '';
-    }; // end pushMCQ function
-
-    // establishes structure for SA Q to be inserted into Q array
-    class ShortAnsQuestion {
-        constructor (q, a) { // q = question, a = answer
-            this.q = q;
-            this.a = a;
-            this.type = 'sa'; // sa = short answer
-        }
-    } // end ShortAnsQuestion Class
+    vm.pushMCQ = ModuleCreation.pushMCQ;
+    // end pushMCQ function
 
     // pushes SA Q into Q array
-    vm.pushSAQ = (q, a) => {
-        vm.newSAQuestion = new ShortAnsQuestion(q, a);
-        vm.questions.push(vm.newSAQuestion);
-        console.log('logging vm.newSAQuestion in pushShortAnsQuestion -> ', vm.newSAQuestion);
-
-        // clear SA fields
-        vm.currentSAQ = ''; // SA Question
-        vm.currentSAA = ''; // SA Answer/hints to grade on
-    }; // end pushSAQ function
-
-    // establishes structure for essay class Qs to be pushed into Q array
-    class Essay {
-        constructor (q, a) {
-            this.q = q;
-            this.a = a;
-            this.type = 'essay';
-        }
-    } // end Essay class
+    vm.pushSAQ = ModuleCreation.pushSAQ; 
+    // end pushSAQ function
 
     // pushes Essay question into Q array
-    vm.pushEssayQ = (q, a) => {
-        vm.newEQ = new Essay(q, a);
-        vm.questions.push(vm.newEQ);
-        console.log('logging vm.newEQ in pushEssayQ -> ', vm.newEQ);
+    vm.pushEssayQ = ModuleCreation.pushEssayQ; // end pushEssayQ function
 
-        // clear Essay fields
-        vm.currentEQ = ''; // Essay Question/Topic
-        vm.currentEA = ''; // Essay Answer/hints to grade on
-    }; // end pushEssayQ function
-
-    // creates overarching 'quiz' item which holds name and questions.
-    // entire 'quiz'will be sent to backend for insertion into DB.
-    class Quiz {
-        constructor (name, q){
-            this.name = name; // name is to be a string
-            this.questions = q; // questions will be a class of either multiple choice, short answer, or essay. 
-                                        // questions will be an array of objects of the above questions.
-        }
-    } // end Quiz class
-
-    // creates quiz object using name and questions array.
-    // POST calls to router with quiz object. Response logged and quiz array reset to 0;
-    vm.pushToQuiz = (name, q) => {
-        vm.newQuiz = new Quiz(name, q);
-        vm.quiz.data.push(vm.newQuiz);
-        console.log('logging vm.quiz in pushToquiz => ', vm.quiz);
-        return $http.post('/quizGeneration', vm.quiz)
-        .then((response)=>{
-            console.log('Posted');
-            vm.quiz.data.length = 0; // empties quiz item
-            vm.name = '';
-        })
-        .catch((e)=>{
-            console.log('logging catch error in vm.pushToQuiz', e);
-        });
-    }; // end pushToQuiz function
+    // // creates quiz object using name and questions array.
+    // // POST calls to router with quiz object. Response logged and quiz array reset to 0;
+    // vm.pushToQuiz = ModuleCreation.pushToQuiz; // end pushToQuiz function
 
     // confirm functionality for submit of form
-    vm.showConfirm = function(ev) {
-        vm.status = ''; // status of submission to log if submission is declined.
-        var confirm = $mdDialog.confirm()
-              .title('Confirm Quiz Submission')
-              .textContent('Do you wish to submit this quiz? Quizzes are unable to be edited after submission.')
-              .ariaLabel('Confirm you wish to submit the quiz.')
-              .targetEvent(ev)
-              .ok('YES')
-              .cancel('NO');
-        $mdDialog.show(confirm).then(function() { // following script runs if 'yes' is selected.
-          vm.status = 'Form submitted.';
-          vm.pushToQuiz(vm.name, vm.questions);
-        }, function() { // status updated and logged if form is not submitted.
-          vm.status = 'Form not submitted.'; 
-          console.log(vm.status);
-          
-        });
-      };
+    vm.showConfirm = ModuleCreation.showConfirm;
 });
--- server/public/scripts/services/moduleCreation.service.js
+++ server/public/scripts/services/moduleCreation.service.js
@@ -1,6 +1,149 @@
-myApp.service('ModuleCreation', function($http){
+myApp.service('ModuleCreation', function ($http, $mdDialog) {
     var sv = this;
 
 
+    /* BEGIN QUIZ GENERATION SECTION OF CODE */
+
+    // Code Readability notes:
+    // q = question
+    // a = answer
+    // sa = short answer
+    // mc = multiple choice
+    // ca = correct answer (for multiple choice)
+    // e = essay (like SA, but longer char limit)
+
+    sv.quiz = {
+        data: []
+    };
+    sv.name = '';
+    sv.currentQType = '';
+    sv.questions = []; // holds questions to push into quiz
+    sv.currentMCQ = ''; // MC Question
+    sv.currentMCA1 = ''; // MC Answers 1-4 below
+    sv.currentMCA2 = '';
+    sv.currentMCA3 = '';
+    sv.currentMCA4 = '';
+    sv.currentMCCA = ''; // MCCA = Multiple Choice Correct Answer
+    sv.currentSAQ = ''; // SA Question
+    sv.currentSAA = ''; // SA Answer/hints to grade on
+    sv.currentEQ = ''; // Essay Question/Topic
+    sv.currentEA = ''; // Essay Answer/hints to grade on
+
+    // classes are created above functions, as functions contain variables referencing classes, ans classes do not hoist.
+
+    // establishes structure for multiple choice question to be inserted into questions array
+    class MCQuestion {
+        constructor(q, a1, a2, a3, a4, ca) {
+            this.q = q;
+            this.a1 = a1;
+            this.a2 = a2;
+            this.a3 = a3;
+            this.a4 = a4;
+            this.ca = ca; // ca = correct answer
+            this.type = 'mc';
+        }
+    } // end MCQuestion class
+
+    // establishes structure for SA Q to be inserted into Q array
+    class ShortAnsQuestion {
+        constructor(q, a) { // q = question, a = answer
+            this.q = q;
+            this.a = a;
+            this.type = 'sa'; // sa = short answer
+        }
+    } // end ShortAnsQuestion Class
+
+    // establishes structure for essay class Qs to be pushed into Q array
+    class Essay {
+        constructor(q, a) {
+            this.q = q;
+            this.a = a;
+            this.type = 'essay';
+        }
+    } // end Essay class
+
+    // creates overarching 'quiz' item which holds name and questions.
+    // entire 'quiz'will be sent to backend for insertion into DB.
+    class Quiz {
+        constructor(name, q) {
+            this.name = name; // name is to be a string
+            this.questions = q; // questions will be a class of either multiple choice, short answer, or essay. 
+            // questions will be an array of objects of the above questions.
+        }
+    } // end Quiz class
+
+    // pushes MC Q into Q array
+    sv.pushMCQ = (q, a1, a2, a3, a4, ca) => {
+        sv.newMCQ = new MCQuestion(q, a1, a2, a3, a4, ca);
+        sv.questions.push(sv.newMCQ);
+        console.log('loggin sv.newMCQ in pushMCQuestion -> ', sv.newMCQ);
+
+        // clear the form for the next questions submission
+        sv.currentMCQ = '';
+        sv.currentMCA1 = '';
+        sv.currentMCA2 = '';
+        sv.currentMCA3 = '';
+        sv.currentMCA4 = '';
+        sv.currentMCCA = '';
+    }; // end pushMCQ function
+
+    // pushes SA Q into Q array
+    sv.pushSAQ = (q, a) => {
+        sv.newSAQuestion = new ShortAnsQuestion(q, a);
+        sv.questions.push(sv.newSAQuestion);
+        console.log('logging sv.newSAQuestion in pushShortAnsQuestion -> ', sv.newSAQuestion);
+
+        // clear SA fields
+        sv.currentSAQ = ''; // SA Question
+        sv.currentSAA = ''; // SA Answer/hints to grade on
+    }; // end pushSAQ function
+
+    // pushes Essay question into Q array
+    sv.pushEssayQ = (q, a) => {
+        sv.newEQ = new Essay(q, a);
+        sv.questions.push(sv.newEQ);
+        console.log('logging sv.newEQ in pushEssayQ -> ', sv.newEQ);
+
+        // clear Essay fields
+        sv.currentEQ = ''; // Essay Question/Topic
+        sv.currentEA = ''; // Essay Answer/hints to grade on
+    }; // end pushEssayQ function
+
+    // creates quiz object using name and questions array.
+    // POST calls to router with quiz object. Response logged and quiz array reset to 0;
+    sv.pushToQuiz = (name, q) => {
+        sv.newQuiz = new Quiz(name, q);
+        sv.quiz.data.push(sv.newQuiz);
+        console.log('logging sv.quiz in pushToquiz => ', sv.quiz);
+        return $http.post('/quizGeneration', sv.quiz)
+            .then((response) => {
+                console.log('Posted');
+                sv.quiz.data.length = 0; // empties quiz item
+                sv.name = '';
+            })
+            .catch((e) => {
+                console.log('logging catch error in sv.pushToQuiz', e);
+            });
+    }; // end pushToQuiz function
+
+    // confirm functionality for submit of form
+    sv.showConfirm = function (ev) {
+        sv.status = ''; // status of submission to log if submission is declined.
+        var confirm = $mdDialog.confirm()
+            .title('Confirm Quiz Submission')
+            .textContent('Do you wish to submit this quiz? Quizzes are unable to be edited after submission.')
+            .ariaLabel('Confirm you wish to submit the quiz.')
+            .targetEvent(ev)
+            .ok('YES')
+            .cancel('NO');
+        $mdDialog.show(confirm).then(function () { // following script runs if 'yes' is selected.
+            sv.status = 'Form submitted.';
+            sv.pushToQuiz(sv.name, sv.questions);
+        }, function () { // status updated and logged if form is not submitted.
+            sv.status = 'Form not submitted.';
+            console.log(sv.status);
+
+        });
+    };
 
 });
