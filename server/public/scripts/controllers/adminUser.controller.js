2ee59f875005f261afd6232f39d9a12366bf3291
Eric Friday <ericfriday@gmail.com>
2017-11-05 16:31:32 -0600

    returning list of schools from db to controller on page load.

--- server/public/scripts/controllers/adminUser.controller.js
+++ server/public/scripts/controllers/adminUser.controller.js
@@ -1,6 +1,37 @@
-myApp.controller('AdminUserController', function(UserService) {
+myApp.controller('AdminUserController', function(UserService, $http) {
     console.log('AdminUserController created');
     const vm = this;
+
+    vm.schoolList = [];
+    vm.schoolName = '';
+    vm.teacherEmail = '';
+    vm.teacherName = '';
+
+    // Creates brand new, non-existing school
+    vm.addSchool = (name) => {
+        console.log('inside addSchool (name)', name);
+        // need to call list of schools to update the view for adding teacher. Need to re-run school GET function.
+        vm.getSchools();
+    };
+
+    // creates brand new, non-existing teacher from name and e-mail
+    vm.addTeacher = (name, email, school) => {
+        console.log('inside addTeacher (name, email)', name, email, school);
+    };
+
+    vm.getSchools = () => {
+        return $http.get('/teacher/schools')
+        .then((res) => {
+            vm.schoolList = res.data.rows;
+            console.log('logging vm.schoolList -> ', vm.schoolList);
+        }, (err) => {
+            console.log('logging err in getSchools -> ', err);
+        })
+        .catch((reason => {
+            console.log('error reason in getSchools -> ', reason);
+        }));
+    };
+
 
 
   });
--- server/routes/teacher.router.js
+++ server/routes/teacher.router.js
@@ -3,33 +3,52 @@ const router = express.Router();
 const pool = require('../modules/pool.js');
 
 router.post('/addTeacher', function (req, res) {
-    console.log('in add teacher post req:', req);
+  console.log('in add teacher post req:', req);
 
-    pool.connect(function(err, client, done) {
-        let query = "INSERT INTO teachers (name, email, password) VALUES ($1, $2, $3)";
-        let saveTeacher = {
-            name: req.body.name,
-            email: encryptLib.encryptPassword(req.body.email),
-            password: encryptLib.encryptPassword(req.body.password)            
-        };
-        
-        if(err) {
-          console.log("Error connecting: ", err);
+  pool.connect(function (err, client, done) {
+    let query = "INSERT INTO teachers (name, email, password) VALUES ($1, $2, $3)";
+    let saveTeacher = {
+      name: req.body.name,
+      email: encryptLib.encryptPassword(req.body.email),
+      password: encryptLib.encryptPassword(req.body.password)
+    };
+
+    if (err) {
+      console.log("Error connecting: ", err);
+      res.sendStatus(500);
+    }
+    client.query(query, [saveTeacher.name, saveTeacher.email, saveTeacher.password],
+      function (err, result) {
+        client.end();
+
+        if (err) {
+          console.log("Error inserting data: ", err);
           res.sendStatus(500);
+        } else {
+          res.send(result).status(203);
         }
-        client.query(query,[saveTeacher.name, saveTeacher.email, saveTeacher.password],
-            function (err, result) {
-              client.end();
-    
-              if(err) {
-                console.log("Error inserting data: ", err);
-                res.sendStatus(500);
-              } else {
-                res.sendStatus(201);
-              }
-            });
       });
+  });
 });
 
+router.get('/schools', (req, res) => {
+  // console.log('Inside schools of teacher.router.js');
+  pool.connect(function (err, client, done) {
+    let queryString = "SELECT * FROM schools;";
+    if (err) {
+      console.log("Error connecting: ", err);
+      res.sendStatus(500);
+    }
+    client.query( queryString, (err, result) => {
+        client.end();
+        if (err) {
+          console.log("Error querying data in /schools GET route: ", err);
+          res.sendStatus(500);
+        } else {
+          res.send(result).status(200);
+        }
+      });
+  });
+});
 
 module.exports = router;
