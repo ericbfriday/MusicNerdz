32a86d5435877291500917893824cf964ff1ae89
Mike Ekert <mikeekert@gmail.com>
2017-11-05 12:53:11 -0600

    fixed lazy code

--- server/public/scripts/controllers/login.controller.js
+++ server/public/scripts/controllers/login.controller.js
@@ -48,7 +48,7 @@ myApp.controller('LoginController', function ($http, $location, UserService) {
           })
           .catch(function (response) {
             console.log('LoginController -- registerUser -- error');
-            vm.message = "Please try again."
+            vm.message = "Please try again.";
           });
       }
     };
