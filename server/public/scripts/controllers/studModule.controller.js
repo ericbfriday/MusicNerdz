4c7824a908db76c6ea088c8b7e9cc5e8c4e6b989
Aurora Hager <roryy18@gmail.com>
2017-11-06 10:47:45 -0600

    removed excess in vendors

--- server/public/scripts/controllers/studModule.controller.js
+++ server/public/scripts/controllers/studModule.controller.js
@@ -1,6 +1,6 @@
 myApp.controller('StudentModuleController', function (UserService) {
     console.log('StudentModuleController created');
     var vm = this;
-    vm.video1 = 'https://www.youtube.com/watch?v=tvTRZJ-4EyI';
+    vm.video1 = 'https://www.youtube.com/watch?v=N0DcVbSYWUA';
     vm.userService = UserService;
 });
