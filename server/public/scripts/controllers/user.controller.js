0e053884a3add8e818094d02433f91a9dfc83d60
Mike Ekert <mikeekert@gmail.com>
2017-11-06 15:07:34 -0600

    carousel

--- server/public/scripts/controllers/user.controller.js
+++ server/public/scripts/controllers/user.controller.js
@@ -1,6 +1,19 @@
-myApp.controller('UserController', function(UserService) {
-  console.log('UserController created');
-  var vm = this;
-  vm.userService = UserService;
-  vm.userObject = UserService.userObject;
-});
+myApp
+  .controller('UserController', function (UserService, $scope) {
+    console.log('UserController created');
+    var vm = this;
+    vm.userService = UserService;
+    vm.userObject = UserService.userObject;
+
+    vm.arrayData = [
+      {
+        src: 'http://s0.limitedrun.com/images/1248348/daisy-4fa4818edd221.jpg'
+      }, {
+        src: 'http://s0.limitedrun.com/images/1248348/daisy-4fa4818edd221.jpg'
+      }, {
+        src: 'http://s0.limitedrun.com/images/1248348/daisy-4fa4818edd221.jpg'
+      }, {
+        src: 'http://s0.limitedrun.com/images/1248348/daisy-4fa4818edd221.jpg'
+      }
+    ];
+  });
