1da0f7eaffbf62b6f843e4f629799f3ab3ca02c1
Eric Friday <ericfriday@gmail.com>
2017-11-17 13:02:14 -0600

    preparing merge

--- server/public/scripts/services/student.service.js
+++ server/public/scripts/services/student.service.js
@@ -1,4 +1,3 @@
-'use strict';
 myApp.service('StudentService', function ($http) {
     //GLOBALS
     const sv = this;
@@ -6,7 +5,7 @@ myApp.service('StudentService', function ($http) {
         data: []
     };
 
-    sv.userObject = [];
+        sv.userObject=[];
 
         sv.saQuestions = {
             data: []
@@ -27,6 +26,16 @@ myApp.service('StudentService', function ($http) {
         this.question = question;
             this.id = id
     } //END constructor 
+    
+        // constructor to create multiple choice objects
+        function McQs(question, a, b, c, d, correct) {
+            this.question = question;
+            this.a = a;
+            this.b = b;
+            this.c = c;
+            this.d = d;
+            this.correct = correct;
+        } //END constructor
 
         //constructor to create history events
         function Hist(title, desc) {
@@ -78,7 +87,6 @@ myApp.service('StudentService', function ($http) {
     //Function to get modules form server
     sv.getMod = function (id) {
         //Temp arrays to hold questions
-        console.log("module ID: ", id);
         let tempSA = [];
         let tempMC = [];
         let tempHist = [];
@@ -116,99 +124,40 @@ myApp.service('StudentService', function ($http) {
         }) //END $http.then
     } //END getMod
 
-    // function to get student grade info back from router
+    // function to get student grades info back from router
     sv.getGrades = function () {
         //temp arrays to hold grades for each module
         let tempLesson5 = [];
         let tempLesson2 = [];
-        //$http get request
-        $http
-            .get('/student/getGrades')
-            .then(function (resp) {
-                console.log('response in service:', resp);
-                // sv.studGrades.data = resp.data loop though response
-                for (let i = 0; i < resp.data.length; i++) {
-                    if (resp.data[i].modules_id === 5) {
-                        tempLesson5.push(resp.data[i] //END if
-                        );
-                    } else if (resp.data[i].modules_id === 2) {
-                        tempLesson2.push(resp.data[i]);
-                    } //END else if
-                } //END for loop
+        // GET request
+        $http.get('/student/getGrades').then(function (resp) {
+            console.log('response in service:', resp);
+            // loop though response
+            for (let i = 0; i < resp.data.length; i++) {
+                if (resp.data[i].modules_id === 5) {
+                    tempLesson5.push(resp.data[i]);
+                } //END if
+                else if (resp.data[i].modules_id === 2) {
+                    tempLesson2.push(resp.data[i]);
+                } //END else if
+            } //END for loop
+        }) //END $http.then
+        sv.studGrades.lesson5 = tempLesson5;
+        sv.studGrades.lesson2 = tempLesson2;
+        console.log('studGRADES:', sv.studGrades);
+    }; //END getGrades
 
-                //Function to get modules form server
-                sv.getMod = function () {
-                    //Temp arrays to hold questions
-                    let tempSA = [];
-                    let tempMC = [];
-                    let tempHist = [];
-                    // GET request
-                    $http.get('/student/getModule').then(function (resp) {
-                        console.log('response in service:', resp);
-                        // set data to global variable
-                        sv.mods.data = resp.data;
-                        // loop through response
-                        for (let i = 0; i < resp.data.length; i++) {
-                            // make history events objects and store in local array
-                            let event = new Hist(resp.data[i].history_title, resp.data[i].history_desc);
-                            tempHist.push(event);
-                            // if the question is Short Answer
-                            if (resp.data[i].type === 'sa') {
-                                // Make new SaQ object from response data for sas
-                                let saQuestion = new SaQs(resp.data[i].question, resp.data[i].id)
-                                //push it to the temp array
-                                tempSA.push(saQuestion);
-                            } //END if
-                            // if the question is Multiple Choice
-                            else if (resp.data[i].type === 'mc') {
-                                // Make new McQ object from response data for mcs
-                                let question = new McQs(resp.data[i].question, resp.data[i].a, resp.data[i].b, resp.data[i].c, resp.data[i].d, resp.data[i].correct, resp.data[i].id);
-                                // push new object into temp array
-                                tempMC.push(question);
-                            } //END else if
-                        } //END for loop
-                        // set globals to value of return from function without duplicates
-                        sv.mcQuestions.data = removeDupes(tempMC, 'question');
-                        sv.histEvents.data = removeDupes(tempHist, 'title');
-                        sv.saQuestions.data = removeDupes(tempSA, 'question')
-                    }); //END $http.then
-                }; //END getMod
+    // function to send quiz responses to server -> DB
+    sv.submitQuiz = function ( resps ) {
+        console.log(resps );
+        // POST request
+        $http({
+            method: 'POST',
+            url: '/student/quiz',
+            data: resps
+        }).then(function (response) {
+            console.log('GET response:', response);
+        })//END $http.then
+    }//END submitQuiz
 
-                // function to get student grades info back from router
-                sv.getGrades = function () {
-                    //temp arrays to hold grades for each module
-                    let tempLesson5 = [];
-                    let tempLesson2 = [];
-                    // GET request
-                    $http.get('/student/getGrades').then(function (resp) {
-                        console.log('response in service:', resp);
-                        // loop though response
-                        for (let i = 0; i < resp.data.length; i++) {
-                            if (resp.data[i].modules_id === 5) {
-                                tempLesson5.push(resp.data[i]);
-                            } //END if
-                            else if (resp.data[i].modules_id === 2) {
-                                tempLesson2.push(resp.data[i]);
-                            } //END else if
-                        } //END for loop
-                    }); //END $http.then
-                    sv.studGrades.lesson5 = tempLesson5;
-                    sv.studGrades.lesson2 = tempLesson2;
-                    console.log('studGRADES:', sv.studGrades);
-                }; //END getGrades
-
-                // function to send quiz responses to server -> DB
-                sv.submitQuiz = function (resps) {
-                    console.log(resps);
-                    // POST request
-                    $http({
-                        method: 'POST',
-                        url: '/student/quiz',
-                        data: resps
-                    }).then(function (response) {
-                        console.log('GET response:', response);
-                    }); //END $http.then
-                }; //END submitQuiz
-            });
-    };
-});
+}); //END service
