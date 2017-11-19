c3105680ac967215a6cb746be4405d52564ef308
Aurora Hager <roryy18@gmail.com>
2017-11-19 13:33:18 -0600

    reomoved commented queries in student router

--- server/routes/student.router.js
+++ server/routes/student.router.js
@@ -233,17 +233,4 @@ router.get('/modules/:id', function (req, res) {
   }); //END pool.connect
 }); //END router GET
 
-
-
-//NEW SQL QUERY::
-// SELECT  resources.link, resources.description AS res_desc, resources.type AS res_type, resources.title AS res_title, questions.id, questions.question, questions.type, questions.a, questions.b, questions.c, questions.d, questions.correct, modules.description AS mod_desc, modules.title AS mod_title, modules.album, modules.artist, modules.year, modules.lyrics, modules.video, history.description AS history_desc, history.title AS history_title, tags.type AS tag_type FROM resources, questions JOIN modules ON questions.modules_id = modules.id JOIN modules_history ON modules.id = modules_history.modules_id JOIN history ON modules_history.history_id = history.id JOIN history_tags ON history.id = history_tags.history_id JOIN tags ON history_tags.tags_id = tags.id WHERE modules.id = 5 ORDER BY questions.question;
-
-//CURRENT Query
-// SELECT questions.id, questions.question, questions.type, questions.a, questions.b, questions.c, questions.d, questions.correct, modules.description AS mod_desc, modules.title AS mod_title, modules.album, modules.artist, modules.year, modules.lyrics, modules.video, history.description AS history_desc, history.title AS history_title, tags.type FROM questions JOIN modules ON questions.modules_id = modules.id JOIN modules_history ON modules.id = modules_history.modules_id JOIN history ON modules_history.history_id = history.id JOIN history_tags ON history.id = history_tags.history_id JOIN tags ON history_tags.tags_id = tags.id WHERE modules.id = 5 ORDER BY questions.question;
-
-
-//OLD QUERY:
-// SELECT questions.id, questions.question, questions.type, questions.a, questions.b, questions.c, questions.d, questions.correct, modules.description AS mod_desc, modules.title AS mod_title, modules.album, modules.artist, modules.year, modules.lyrics, modules.video, history.description AS history_desc, history.title AS history_title FROM questions JOIN modules ON questions.modules_id = modules.id JOIN modules_history ON modules.id = modules_history.modules_id JOIN history ON modules_history.history_id = history.id WHERE modules.id = $1 ORDER BY questions.question;
-
-
 module.exports = router;
